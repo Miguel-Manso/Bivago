824fc5c0272a6c01408cb38646dcc6da0d98ca08
Miguel-Manso <miguelfilipemanso13@gmail.com>
2022-06-19 21:17:20 -0300

    adição filter de caracter

--- src/views/pages/Cadastro.js
+++ src/views/pages/Cadastro.js
@@ -6,16 +6,16 @@ const Cadastro = () => {
     return ( 
         <>
             <div className='container-cadastro'>
-                    <form className='form-cadastro'>
+                    <form  className='form-cadastro'>
                         <h2 class="title-cadastro" >CADASTRO</h2>
                         <input id='cadastro-nome' type="text" placeholder="Digite seu nome" />
                         <input id='cadastro-tel'type="tel" placeholder="Número de celular" />
-                        <input id='cadastro-email'type="email" placeholder="Digite seu e-mail" />
+                        <input id='cadastro-email'type="email" placeholder="Digite seu e-mail" required maxLength={25}/>
                         <input id='cadastro-senha'type="password" placeholder='Criar uma senha' />
                         <input id='cadastro-senha'type="password" placeholder='Repetir sua senha' />
                         <br />
-                        <input id='cadastro-submit'type="submit" class="enviar" value="Cadastrar"  />
 						
+                
                     </form>
 					
 
